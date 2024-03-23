c3342ec6dde28c1a8b4d68feafd373e8052e9103
mertalev <101130780+mertalev@users.noreply.github.com>
2024-03-23 18:41:07 -0400

    use embedding in db instead of fetching

--- server/src/interfaces/search.interface.ts
+++ server/src/interfaces/search.interface.ts
@@ -134,7 +134,6 @@ export interface SearchExifOptions {
 export interface SearchEmbeddingOptions {
   embedding: Embedding;
   userIds: string[];
-  maxDistance?: number;
 }
 
 export interface SearchPeopleOptions {
@@ -176,6 +175,13 @@ export type SmartSearchOptions = SearchDateOptions &
 export interface FaceEmbeddingSearch extends SearchEmbeddingOptions {
   hasPerson?: boolean;
   numResults: number;
+  maxDistance?: number;
+}
+
+export interface AssetDuplicateSearch {
+  assetId: string;
+  userIds: string[];
+  maxDistance?: number;
 }
 
 export interface FaceSearchResult {
@@ -193,7 +199,7 @@ export interface ISearchRepository {
   init(modelName: string): Promise<void>;
   searchMetadata(pagination: SearchPaginationOptions, options: AssetSearchOptions): Paginated<AssetEntity>;
   searchSmart(pagination: SearchPaginationOptions, options: SmartSearchOptions): Paginated<AssetEntity>;
-  searchDuplicates(options: SearchEmbeddingOptions): Promise<AssetDuplicateResult[]>;
+  searchDuplicates(options: AssetDuplicateSearch): Promise<AssetDuplicateResult[]>;
   searchFaces(search: FaceEmbeddingSearch): Promise<FaceSearchResult[]>;
   upsert(smartInfo: Partial<SmartInfoEntity>, embedding?: Embedding): Promise<void>;
   searchPlaces(placeName: string): Promise<GeodataPlacesEntity[]>;
